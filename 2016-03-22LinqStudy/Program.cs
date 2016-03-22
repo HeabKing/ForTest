d2ec9b998b7c5dd0a8e7af028372ed5fffdb5b1d
HeabKing <HeabKing@gmail.com>
2016-03-22 15:36:54 +0800

    添加Linq学习项目

---
+++ 2016-03-22LinqStudy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_03_22LinqStudy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var db = new dbDataContext();
+			IQueryable<TblSurvey> result = from m in db.TblSurvey
+						 select m;
+			Console.WriteLine(result);
+		}
+	}
+}
