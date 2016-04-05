dcac2b6dd3b2dcf7a922e3b52b13a2ba2b74110f
HeabKing <HeabKing@gmail.com>
2016-04-05 09:25:26 +0800

    添加对HtmlAgility的学习

---
+++ 2016-04-01HtmlAgilityPack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace _2016_04_01HtmlAgilityPack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HttpClient client = new HttpClient();
+			var htmlTask = client.GetStringAsync("http://www.baidu.com");
+			var html = htmlTask.Result;
+
+			html = $"<div id='justForGetText'>{html}</div>";
+
+			HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+			doc.LoadHtml(html);
+			var result = doc.GetElementbyId("justForGetText").InnerText;
+			//Console.WriteLine(result);
+
+			string str = @"毕达哥拉斯，一个神奇的学派。他与他们的传说，贯穿于我们基础数论、几何的研究当中。现在就让我们随着教徒希帕索斯的脚步，走近这个学（hei）派（bang）……
+
+
+
+
+/ a12.html>";
+			str = Regex.Replace(str, @"\s+", " ");
+			Console.WriteLine(str);
+		}
+	}
+}
---
+++ ForTest/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autofac;
+using Autofac.Core;
+using Dapper;
+
+namespace ForTest
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Thread thread = new Thread(Run);
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();
+		}
+
+		public static void Run()
+		{
+			WebBrowser web = new WebBrowser { Url = new Uri("http://www.baidu.com") };
+			web.DocumentCompleted += Web_DocumentCompleted;
+		}
+
+		private static void Web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			var web = sender as WebBrowser;
+			if (web?.ReadyState != WebBrowserReadyState.Complete) { return; }
+			if (web.Document == null) return;
+			foreach (var item in web.Document.Links)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+
+	#region 2016-03-16 DI
+	//public class Program
+	//{
+	//	interface IOutput
+	//	{
+	//		void Write(string content);
+	//	}
+
+	//	class ConsoleOutput : IOutput
+	//	{
+	//		public void Write(string content)
+	//		{
+	//			Console.WriteLine(content);
+	//		}
+	//	}
+
+	//	interface IShowToday
+	//	{
+	//		void Show();
+	//	}
+
+	//	class ShowToday : IShowToday
+	//	{
+	//		public IOutput Output { get; set; }
+	//		public ShowToday(IOutput output)
+	//		{
+	//			Output = output;
+	//		}
+	//		public ShowToday()
+	//		{
+
+	//		}
+	//		public void Show()
+	//		{
+	//			Output.Write(DateTime.Now.ToString());
+	//		}
+	//	}
+
+	//	public static void Main()
+	//	{
+	//		var builder = new ContainerBuilder();
+	//		builder.RegisterType<ConsoleOutput>().As<IOutput>();
+	//		// 使用lambda表达式注册 - 属性注入
+	//		//builder.Register(c => new ShowToday { Output = c.Resolve<IOutput>()}).As<IShowToday>();
+	//		//builder.Register(c => new ShowToday()).OnActivated(e => e.Instance.Output = e.Context.Resolve<IOutput>()).As<IShowToday>(); // For 循环依赖
+	//		// 反射组件注册 - 属性注入
+	//		//builder.RegisterType<ShowToday>().PropertiesAutowired().As<IShowToday>();	// 使用PropertiesAutowired()实现自动连接 条件: 在反射组件注册使用
+	//		builder.RegisterType<ShowToday>().WithProperty("OutPut", new ConsoleOutput()).As<IShowToday>();
+	//		var container = builder.Build();
+
+	//		var today = container.Resolve<IShowToday>();
+	//		today.Show();
+	//	}
+
+	//} 
+	#endregion
+
+	#region 2016-03-15 Dapper
+	//public class Program
+	//{
+	//	public static void Main()
+	//	{
+	//		using (SqlConnection conn = new SqlConnection("Data Source=qds150599512.my3w.com;Initial Catalog=qds150599512_db;User Id=qds150599512;Password=he394899990"))
+	//		{
+	//			var guid = Guid.NewGuid();
+	//			var result = conn.QueryMultiple(@"SELECT 1 A,2 B;SELECT 3 C,4 D,5 E");
+	//			var r1 = result.Read();
+	//			var r2 = result.Read();
+	//			//var r3 = result.Read();	// 异常
+	//		}
+	//	}
+	//} 
+	#endregion
+
+	#region 2016-03-15 DI
+	//public class Program
+	//{
+	//	public interface IOutPut
+	//	{
+	//		void Write(string content);
+	//	};
+
+	//	public class ConsoleOutput : IOutPut
+	//	{
+	//		public void Write(string content)
+	//		{
+	//			Console.WriteLine(content);
+	//		}
+	//	}
+
+	//	public static IContainer StartUp()
+	//	{
+	//		// 容器创建器
+	//		var builder = new ContainerBuilder();
+
+	//		// 将自己公开
+	//		builder.RegisterType<ConsoleOutput>();
+
+	//		return builder.Build();
+	//	}
+
+	//	// 通过IContainer创建的是整个应用程序声明周期的组件
+	//	private static IContainer Container { get; set; }
+
+	//	static void Main()
+	//	{
+	//		Container = StartUp();
+	//		ConsoleOutput console = Container.Resolve<ConsoleOutput>();
+	//		console.Write("Hello World");
+	//	}
+	//} 
+	#endregion
+
+}
+
