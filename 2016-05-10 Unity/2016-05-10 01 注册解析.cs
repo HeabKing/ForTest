fd436c54f1b381c0d80bd3e4df85141109309a33
HeabKing <HeabKing@gmail.com>
2016-05-10 15:38:04 +0800

    Unity

---
+++ 2016-05-10 Unity/2016-05-10 01 注册解析.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace _2016_05_10_Unity
+{
+	class Program
+	{
+		static void Main1(string[] args)
+		{
+			// 建立Unity容器
+			IUnityContainer container = new UnityContainer();
+			// 向Unity容器注册类型
+			container.RegisterType<ISayHello, SayHelloInEnglist>();
+			// 在程序某处要求解析类型, 已取得组件的实例
+			ISayHello hello = container.Resolve<ISayHello>();
+			// 调用组件的方法
+			hello.Run();
+
+			Console.ReadKey();
+		}
+
+		interface ISayHello
+		{
+			void Run();
+		}
+
+		class SayHelloInEnglist : ISayHello
+		{
+			public void Run()
+			{
+				Console.WriteLine("Hello Unity!");
+			}
+		}
+
+		class SayHelloInChinese : ISayHello
+		{
+			public void Run()
+			{
+				Console.WriteLine("哈罗, Unity!");
+			}
+		}
+	}
+}
---
+++ 2016-05-10 Unity/2016-05-10 02 发送信息.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_05_10_Unity
+{
+	public interface IMessageService
+	{
+		void SendMessage(string to, string msg);
+	}
+
+	public class EmailService : IMessageService
+	{
+		public void SendMessage(string to, string msg)
+		{
+			Console.WriteLine($"通过EmailService发送邮件給{to}");
+		}
+	}
+
+	public class SmsService : IMessageService
+	{
+		public void SendMessage(string to, string msg)
+		{
+			Console.WriteLine($"通过SmsService发送短信給{to}");
+		}
+	}
+
+	public interface INotificationManager
+	{
+		void Notify(string to, string msg);
+	}
+
+	public class NotificationManager : INotificationManager
+	{
+		private readonly IMessageService _msgService = null;
+		// 从构造函数注入讯息服务对象
+		public NotificationManager(IMessageService svc)
+		{
+			_msgService = svc;
+		}
+		// 利用讯息服务来发送讯息給指定对象
+		public void Notify(string to, string msg)
+		{
+			_msgService.SendMessage(to, msg);
+		}
+	}
+}
