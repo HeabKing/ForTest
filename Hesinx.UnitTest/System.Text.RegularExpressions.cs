5b5f986c05c1149e4d010c6e3d332e35b648173d
HeabKing <HeabKing@gmail.com>
2016-05-26 18:29:19 +0800

    添加hesinx的单元测试

---
+++ Hesinx.UnitTest/System.Text.RegularExpressions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hesinx.UnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			string email1 = "heabking@163.com";
+			string email2 = "39489990@vip.qq.com";
+			string email3 = "fdas.32@qq.com";
+			string email4 = "fda@qq";
+			string email5 = "fadfasfsqq.com";
+
+			string pattern = ((Regex) null).GetEmailPattern();
+			Assert.IsTrue(Regex.IsMatch(email1, pattern));
+			Assert.IsTrue(Regex.IsMatch(email2, pattern));
+			Assert.IsTrue(Regex.IsMatch(email3, pattern));
+			Assert.IsFalse(Regex.IsMatch(email4, pattern));
+			Assert.IsFalse(Regex.IsMatch(email5, pattern));
+		}
+	}
+}
--- Hesinx/Extension/System.Text.RegularExpressions/RegexExtension.cs
+++ Hesinx/Extension/System.Text.RegularExpressions/RegexExtension.cs
@@ -27,5 +27,16 @@ namespace System.Text.RegularExpressions
 		{
 			return @"^(https?|ftp)://[^\s/$.?#].[^\s]*$";
 		}
+
+		/// <summary>
+		/// 邮箱正则 https://msdn.microsoft.com/en-us/library/01escwtf(v=vs.110).aspx
+		/// </summary>
+		/// <param name="regex"></param>
+		/// <returns></returns>
+		public static string GetEmailPattern(this Regex regex)
+		{
+			return @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+			       @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+		}
 	}
 }
