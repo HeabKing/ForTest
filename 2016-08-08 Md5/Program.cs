8e58c190b9755f2995d5a13d5e5a2427953cd387
HeabKing <HeabKing@qq.com>
2016-08-08 18:09:27 +0800

    Md5

---
+++ 2016-08-08 Md5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_08_08_Md5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// 32位 小写
+			Func<string, string> md5 = input => System.Security.Cryptography.MD5.Create()
+				.ComputeHash(Encoding.UTF8.GetBytes(input))
+				.Aggregate(new StringBuilder(), (sb, index) => sb.Append(index.ToString("x2")))
+				.ToString();
+			Console.WriteLine(md5("123456"));
+		}
+	}
+}
