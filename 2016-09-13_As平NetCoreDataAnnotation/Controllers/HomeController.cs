97e5beda3e9963b64e1875fb9a2e63e977fba2c0
HeabKing <heabking@gmail.com>
2016-09-13 20:56:16 +0800

    jquery-validator project add

---
+++ 2016-09-13_As平NetCoreDataAnnotation/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2016_09_13_As平NetCoreDataAnnotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Model.StudentViewModel student)
+        {
+            return Content(ModelState.IsValid.ToString());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2016-09-13_As平NetCoreDataAnnotation/Model/StudentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2016_09_13_As平NetCoreDataAnnotation.Model
+{
+    public class StudentViewModel
+    {
+        [Required]
+        [Range(2, 3)]
+        public string Name { get; set; }
+    }
+}
